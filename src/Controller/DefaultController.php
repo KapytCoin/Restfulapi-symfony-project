7a4ba09251908dc16d432844040bd343a243a312
Ilya Latypov <ilya.latypov2002@yandex.ru>
2024-10-03 18:02:20 +0500

    feat: added product entity, default controller and connected pgsql
    fix: microfixes

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_default')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->json(['test' => 'hello']);
+    }
+}

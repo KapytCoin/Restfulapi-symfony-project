7d29d5b6dd5bafe948285472396ed3a5708d47cc
Ilya Latypov <ilya.latypov2002@yandex.ru>
2024-10-14 20:36:56 +0500

    feat: added RefreshToken
    fix: fix entry point JWT

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];

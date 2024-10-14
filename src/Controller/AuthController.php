dbb5072b0dd5494d3a1eba2fb5c6621e9e29159d
Ilya Latypov <ilya.latypov2002@yandex.ru>
2024-10-14 17:25:26 +0500

    feat: added registration and authentication by JWT

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Attribute\RequestBody;
+use App\Model\SignUpRequest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Service\SignUpService;
+
+class AuthController extends AbstractController
+{
+    public function __construct(private SignUpService $signUpService)
+    {
+    }
+
+    #[Route('/api/v1/auth/signUp', methods: ['POST'])]
+    public function signUp(#[RequestBody] SignUpRequest $signUpRequest): Response
+    {
+        return $this->json($this->signUpService->signUp($signUpRequest));
+    }
+}
